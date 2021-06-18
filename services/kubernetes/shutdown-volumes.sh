89b6e4f2f30525193a6a85d7c3abec444814f6a9
Ho Kim <great.ho.kim@gmail.com>
2021-06-18 20:23:35 +0900

    Update

--- services/kubernetes/shutdown-volumes.sh
+++ services/kubernetes/shutdown-volumes.sh
@@ -5,7 +5,6 @@ for volume in $volumes; do
     sudo sgdisk --zap-all $volume && sync
     sudo dd if=/dev/zero of=$volume bs=1M count=100 conv=direct,dsync && sync
     sudo blkdiscard $volume && sync
-    sudo partprobe $volume && sync
 done
 
 # Cleanup Rook configuration
@@ -13,3 +12,8 @@ done
 ls /dev/mapper/ceph-* | xargs -I% -- sudo dmsetup remove %
 sudo rm -rf /dev/ceph-*
 sudo rm -rf /dev/mapper/ceph--*
+
+# Update tables
+for volume in $volumes; do
+    sudo partprobe $volume && sync
+done
