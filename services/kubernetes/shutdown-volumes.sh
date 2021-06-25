1ec8834427c3d5e918c793fc0de66c5002959b36
Ho Kim <great.ho.kim@gmail.com>
2021-06-25 08:45:15 +0000

    Fix a bug

--- services/kubernetes/shutdown-volumes.sh
+++ services/kubernetes/shutdown-volumes.sh
@@ -3,7 +3,7 @@
 # Cleanup LVMs
 for volume in $volumes; do
     sudo sgdisk --zap-all $volume && sync
-    sudo dd if=/dev/zero of=$volume bs=1M count=100 conv=direct,dsync && sync
+    sudo dd if=/dev/zero of=$volume bs=1M count=100 oflag=direct,dsync && sync
     sudo blkdiscard $volume && sync
 done
 
